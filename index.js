f46d133e7c9548970df33076bad736ee092f1b59
Roy Eriksen <roy.h.eriksen@gmail.com>
2023-05-10 17:23:28 +0800

    updated version of index.js still WIP

--- index.js
+++ index.js
@@ -1,36 +1,80 @@
+import * as readline from 'readline';
+
 import { fetchLunoPrice } from './lib/luno.js';
 import { convertCurrency } from './exchange-rate.js';
 import { getBinanceBTCPriceInUSD } from './lib/binance.js';
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const getUserInput = (question) => {
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            resolve(answer);
+        });
+    });
+};
+
+const askForCurrency = async() => {
+    let currencySelected = await getUserInput("Please input what crypto currency do you want to check? (XBT, ETH, LTC, XRP)");
+    currencySelected = currencySelected.toUpperCase(); // convert input to uppercase 
+    console.log(`You selected ${currencySelected}`)
+
+    const validCurrencies = ['XBT', 'ETH', 'LTC', 'XRP'];
+
+    while (!validCurrencies.includes(currencySelected)) {
+        currencySelected = prompt ('Invaid input. Please enter one of XBT, ETH, LTC, XRP');
+        currencySelected = currencySelected.toUpperCase();
+    }
+}
+
+
 async function main() {
-    const lunoPrice = await fetchLunoPrice('https://api.luno.com/api/1/ticker?pair=XBTMYR');
-    console.log(`BTCMYR price on Luno: MYR ${lunoPrice}`);
-    
-    const exchangeRate = await convertCurrency();
-    console.log(`USDMYR: ${exchangeRate}`);
-
-    const lunoUSD = lunoPrice / exchangeRate;
-    console.log(`BTCUSD price on Luno: USD ${lunoUSD}`);
-
+    const currencySelected = await askForCurrency();
+    const lunoPrice = await fetchLunoPrice('https://api.luno.com/api/1/ticker?pair=${currencySelected}MYR');
+    const lunoPriceNum = parseFloat(lunoPrice)
+    const lunoPriceStr = `MYR ${lunoPriceNum.toFixed(2)}`;
+    const usdMYR = (await convertCurrency()).toFixed(6);
+    const lunoUSD = lunoPrice / await convertCurrency();
+    const lunoUSDStr = lunoUSD.toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
     const binancePrice = await getBinanceBTCPriceInUSD();
-    console.log(`BTCUSD price on Binance: USD ${binancePrice}`);
+    const binancePriceStr = binancePrice.toFixed(2);
 
-    const priceDifference = lunoUSD - binancePrice;
-    console.log(`Price difference: USD ${priceDifference}`);
+    const priceDifference = (lunoUSD - binancePrice).toFixed(2);
+    const premium = ((lunoUSD / binancePrice - 1) * 100).toFixed(2);
 
-    const premium = priceDifference / binancePrice;
-    const percentage = premium * 100 + "%"
-    console.log(`Luno premium: ${percentage}`);
+    
+    const lunoPriceStrLength = (`BTCMYR price on Luno: ${lunoPriceStr}`).length;
+    const usdMYRLength = (`USDMYR: ${usdMYR}`).length;
+    const lunoUSDStrLength = (`BTCUSD price on Luno: USD ${lunoUSDStr}`).length;
+    const binancePriceStrLength = (`BTCUSD price on Binance: USD ${binancePriceStr}`).length;
+    const priceDifferenceLength = (`Price difference: USD ${priceDifference}`).length;
+    const premiumLength = (`Luno premium: ${premium}`).length;
+
+    const maxLength = Math.max(
+        lunoPriceStrLength,
+        usdMYRLength,
+        lunoUSDStrLength,
+        binancePriceStrLength,
+        priceDifferenceLength,
+        premiumLength
+    );
+    
+
+    console.log((`BTCMYR price on Luno: ${lunoPriceStr}`).padStart(maxLength));
+    console.log((`USDMYR: ${usdMYR}`).padStart(maxLength));
+    console.log((`BTCUSD price on Luno: USD ${lunoUSDStr}`).padStart(maxLength));
+    console.log((`BTCUSD price on Binance: USD ${binancePriceStr}`).padStart(maxLength));
+    console.log((`Price difference: USD ${priceDifference}`).padStart(maxLength));
+    console.log((`Luno premium: ${premium}%`).padStart(maxLength));
+
 }
 
 main();
 
-
-
-
-
-
-// async function main() {
-//     await getBinanceBTCPriceInUSD();
-// }
 
